f1c7d1e416a8b934af4d6e9c4cacfe642f13e7eb
Sijie Jin <jinsijie0318@gmail.com>
2018-07-08 19:06:26 -0400

    update course suggestion

--- GoGrad/app/src/main/java/com/example/gograd/utli/SuggestCoursesModel.java
+++ GoGrad/app/src/main/java/com/example/gograd/utli/SuggestCoursesModel.java
@@ -6,15 +6,41 @@ public class SuggestCoursesModel {
 
     private ArrayList<String> suggestCourses;
 
+    //private database
+
     public SuggestCoursesModel() {
         suggestCourses = new ArrayList<>();
     }
 
-
     public ArrayList<String> getSuggestCourses() {
-        return suggestCourses;
-    }
 
-    
+        ArrayList<String> temp;
+        for (int row = 1; row < 10 ; row++) {
+            /*
+            if (row.ischeck == true) {
+                String[] lines = row.successor.split(",");
+                for(String line: lines) {
+                    for(String prerequest : lines.prerequest.split(",")) {
+                        for (int row = 1; row < total_length; i++) {
+                            if (row.name == prerequest && row.ischeck == true) {
+                                suggestCourses.add(line);
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                for(String prerequest : row.name.prerequest.split(",")) {
+                        for (int row = 1; row < total_length; i++) {
+                            if (row.name == prerequest && row.ischeck == true) {
+                                suggestCourses.add(line);
+                            }
+                        }
+                 }
+            }
+            */
+        }
+        return getSuggestCourses();
+    }
 
 }
