90e922e1ba2533bf3bd62038ea14bcca1d273320
MierShi <miershi2015@gmail.com>
2018-07-12 10:47:54 -0400

    DataAccess and DB update
    DataAccess and DB update

--- GoGrad/app/src/main/java/com/example/gograd/DatabaseAccess.java
+++ GoGrad/app/src/main/java/com/example/gograd/DatabaseAccess.java
@@ -6,17 +6,46 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseAccess {
+    //Tables
+    public static final String Course_TABLE = "CourseDescription";
+    public static final String Checlist_TABLE = "CheckList";
+    //CourseDescription Columns
+    public static final String COL_1 = "_ID";           //TEXT
+    public static final String COL_2 = "CourseName";    //TEXT NOT NULL
+    public static final String COL_3 = "Title";         //TEXT
+    public static final String COL_4 = "Description";   //TEXT NOT NULL
+    public static final String COL_5 = "Prereqs";       //TEXT
+    public static final String COL_6 = "Successor";     //TEXT
+    public static final String COL_7 = "Coreqs";        //TEXT
+    public static final String COL_8 = "OpenNonCS";     //Integer(0/1)
+    public static final String COL_9 = "FALL";          //Integer(0/1/null)
+    public static final String COL_10 = "WINTER";       //Integer(0/1/null)
+    public static final String COL_11 = "SPRING";       //Integer(0/1/null)
+    //CheckList Columns
+    public static final String COL_C1 = "_ID";           //TEXT
+    public static final String COL_C2 = "Name";          //TEXT NOT NULL
+    public static final String COL_C3 = "AdditionalConstraints"; //TEXT
+    public static final String COL_C4 = "CSUnits";       //TEXT
+    public static final String COL_C5 = "ElectiveUnits"; //TEXT
+    public static final String COL_C6 = "NonMathUnits";  //TEXT
+    public static final String COL_C7 = "MathUnits";     //TEXT
+    public static final String COL_C8 = "TotalCS";       //Numeric
+    public static final String COL_C9 = "TotalElective"; //Numeric
+    public static final String COL_C10 = "TotalMath";    //Numeric
+    public static final String COL_C11 = "TotalNonMath"; //Numeric
+    public static final String COL_C12 = "WithOption";   //Integer
+    public static final String COL_C13 = "Link";         //TEXT
+
     private SQLiteOpenHelper openHelper;
     private SQLiteDatabase db;
     private  static DatabaseAccess instance;
-    Cursor c = null;
 
     //provide constructor so that object creation from outside the class is avoided
     private DatabaseAccess(Context context){
         this.openHelper = new DatabaseAssetHelper(context);
     }
 
-    //To return the single instance of database
+    //To return the singleton instance of database
     public static DatabaseAccess getInstance(Context context){
         if(instance == null){
             instance = new DatabaseAccess(context);
@@ -37,14 +66,137 @@ public class DatabaseAccess {
     }
 
     //Method to query and return results
+    //Data from table CourseDescription
     public String getDescription(String name){
-        c = db.rawQuery("select Description from CourseDescription where Name = '"+name+"'",
-                new String[]{});
-        StringBuffer buffer = new StringBuffer();
+        String description = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
         while(c.moveToNext()){
-            String description = c.getString(0);
-            buffer.append(""+description);
+            description = c.getString(c.getColumnIndexOrThrow(COL_4));
         }
-        return buffer.toString();
+        c.close();
+        return description;
+    }
+
+    public String getTitle(String name){
+        String title = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            title = c.getString(c.getColumnIndexOrThrow(COL_3));
+        }
+        c.close();
+        return title;
+    }
+
+    public String getPrereqs(String name){
+        String temp = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            temp = c.getString(c.getColumnIndexOrThrow(COL_5));
+        }
+        c.close();
+        return temp;
+    }
+
+    public String getSuccessor(String name){
+        String description = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            description = c.getString(c.getColumnIndexOrThrow(COL_6));
+        }
+        c.close();
+        return description;
+    }
+
+    public String getCoreqs(String name){
+        String description = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Checlist_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            description = c.getString(c.getColumnIndexOrThrow(COL_7));
+        }
+        c.close();
+        return description;
+    }
+
+    public int getOpenNonCS(String name){
+        int i =-1;
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            i = c.getInt(c.getColumnIndexOrThrow(COL_8));
+        }
+        c.close();
+        return i;
+    }
+
+    //Data from table Checklist
+    public String getCSUnits(String name){
+        String temp = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            temp = c.getString(c.getColumnIndexOrThrow(COL_C4));
+        }
+        c.close();
+        return temp;
+    }
+
+    public String getConstraints(String name){
+        String temp = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            temp = c.getString(c.getColumnIndexOrThrow(COL_C3));
+        }
+        c.close();
+        return temp;
+    }
+
+    public String getElective(String name){
+        String temp = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            temp = c.getString(c.getColumnIndexOrThrow(COL_C5));
+        }
+        c.close();
+        return temp;
+    }
+
+    public String getNonMath(String name){
+        String temp = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            temp = c.getString(c.getColumnIndexOrThrow(COL_C6));
+        }
+        c.close();
+        return temp;
+    }
+
+    public String getMath(String name){
+        String temp = "";
+        String[] params = new String[]{name};
+        Cursor c = db.rawQuery("select * from "+Course_TABLE+" where "+COL_2+" = ?",
+                params);
+        while(c.moveToNext()){
+            temp = c.getString(c.getColumnIndexOrThrow(COL_C7));
+        }
+        c.close();
+        return temp;
     }
 }
--- GoGrad/app/src/main/java/com/example/gograd/DatabaseAssetHelper.java
+++ GoGrad/app/src/main/java/com/example/gograd/DatabaseAssetHelper.java
@@ -1,69 +1,20 @@
 package com.example.gograd;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseAssetHelper extends SQLiteAssetHelper {
-    public static final String TABLE_NAME = "CourseDescription";
-    public static final String COL_1 = "_ID";           //TEXT
-    public static final String COL_2 = "CourseName";    //TEXT NOT NULL
-    public static final String COL_3 = "Title";         //TEXT
-    public static final String COL_4 = "Description";   //TEXT NOT NULL
-    public static final String COL_5 = "Prereqs";       //TEXT
-    public static final String COL_6 = "Successor";     //TEXT
-    public static final String COL_7 = "Coreqs";        //TEXT
-    public static final String COL_8 = "OpenNonCS";     //Integer(0/1)
-    public static final String COL_9 = "FALL";          //Integer(0/1/null)
-    public static final String COL_10 = "WINTER";       //Integer(0/1/null)
-    public static final String COL_11 = "SPRING";       //Integer(0/1/null)
-
     private static final String DATABASE_NAME = "GoGrad.db";
     private static final int DATABASE_VERSION = 1;
 
     public DatabaseAssetHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-
+    /*
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + Course_TABLE);
         onCreate(db);
     }
-
-    public boolean insertData(String name, String surname, String marks) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2, name);
-        contentValues.put(COL_3, surname);
-        contentValues.put(COL_4, marks);
-        long result = db.insert(TABLE_NAME, null, contentValues);
-        return result != -1;
-    }
-    //TODO:: do not use this function
-    public Cursor getAllData() {
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
-        return res;
-    }
-
-    public boolean updateData(String id, String name, String surname, String marks) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_1, id);
-        contentValues.put(COL_2, name);
-        contentValues.put(COL_3, surname);
-        contentValues.put(COL_4, marks);
-        db.update(TABLE_NAME, contentValues, "ID = ?", new String[]{id});
-        return true;
-    }
-
-    public Integer deleteData(String id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete(TABLE_NAME, "ID = ?", new String[]{id});
-    }
-
+    */
 }
