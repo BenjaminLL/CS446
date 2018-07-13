fa1731823ea113f15e6487eed384a4239ead935b
Sijie Jin <jinsijie0318@gmail.com>
2018-07-13 18:34:40 -0400

    update eachChecklist

--- GoGrad/app/src/main/java/com/example/gograd/utli/EachChecklist.java
+++ GoGrad/app/src/main/java/com/example/gograd/utli/EachChecklist.java
@@ -1,26 +1,77 @@
 package com.example.gograd.utli;
 
+import android.content.Context;
+import android.util.Pair;
+
+import com.example.gograd.ChecklistOpenHelper;
+import com.example.gograd.DatabaseAccess;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class EachChecklist {
 
-    private ArrayList<EachCourse> eachCourse;
-    //database
+    private List<Pair<String, ArrayList<EachCourse>>> course;
+    private List<Pair<EachConstraints, ArrayList<EachConstraints>>> constraint;
     private String whichPlan;
+    private Context context;
+    private ChecklistOpenHelper checklistOpenHelper;
 
-    public EachChecklist(String whichPlan) {
 
-        eachCourse = new ArrayList<>();
+    public EachChecklist(String whichPlan, Context context) {
+
+        course = new ArrayList<>();
+        constraint = new ArrayList<>();
         this.whichPlan = whichPlan;
+        this.context = context;
+        ChecklistOpenHelper checklistOpenHelper = new ChecklistOpenHelper(context, "checklist.db", null, 1);
     }
 
-    public ArrayList<EachCourse> getEachCourse() {
-        /*
-        for (int row = 0; row < total_length; i++) {
-           EachCourse temp = new EachCourse(row.name, row.ischeck);
-        }
-        */
-        return eachCourse;
+    public List<Pair<String, ArrayList<EachCourse>>> getCourse() {
+
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(context);
+        databaseAccess.open();
+
+        String key1 = "CS Units";
+        String value1 = databaseAccess.getCSUnits(whichPlan);
+        splitCourses(key1, value1);
+
+        String key2 = "Math Units";
+        String value2 = databaseAccess.getMath(whichPlan);
+        splitCourses(key2, value2);
+
+        String key3 = "Elective Units";
+        String value3 = databaseAccess.getElective(whichPlan);
+        splitCourses(key3, value3);
+
+        String key4 = "Non-Math Units";
+        String value4 = databaseAccess.getNonMath(whichPlan);
+        splitCourses(key4, value4);
+        
+        return course;
     }
+
+    public List<Pair<EachConstraints, ArrayList<EachConstraints>>> getConstraint() {
+        return constraint;
+    }
+
+    private void splitCourses(String key, String value) {
+
+        ArrayList<EachCourse> tempArr = new ArrayList<>();
+        if (value == null) {
+            //EachCourse eachCourse = new EachCourse(whichPlan, line, checklistOpenHelper.getIsCheck(line));
+            Pair<String, ArrayList<EachCourse>> tempPair = new Pair<>(key, tempArr);
+            course.add(tempPair);
+            return;
+        }
+        String[] lines = value.split("\\r?\\n");
+        for (String line : lines) {
+            //EachCourse eachCourse = new EachCourse(whichPlan, line, checklistOpenHelper.getIsCheck(line));
+            //tempArr.add(eachCourse)
+        }
+        Pair<String, ArrayList<EachCourse>> tempPair = new Pair<>(key, tempArr);
+        course.add(tempPair);
+    }
+
 
 }
