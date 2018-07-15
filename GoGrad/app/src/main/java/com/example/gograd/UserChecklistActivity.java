f8c5cc4d76f00d906a8f864771d0012da7351f34
BenjaminLL <szyllll8@gmail.com>
2018-07-14 20:20:11 -0400

    test

--- GoGrad/app/src/main/java/com/example/gograd/UserChecklistActivity.java
+++ GoGrad/app/src/main/java/com/example/gograd/UserChecklistActivity.java
@@ -29,6 +29,7 @@ import java.util.Map;
 
 public class UserChecklistActivity extends AppCompatActivity {
 
+    private EachChecklist checklist;
     private List<Pair<String, ArrayList<EachCourse>>> requiredCourses;
 //    private List<Pair<String, ArrayList<String>>> additionalConstraints;
 
@@ -58,7 +59,11 @@ public class UserChecklistActivity extends AppCompatActivity {
 
         ab.setTitle(title);
 
-
+        /**
+         * get checklist from user database
+         */
+        checklist = new EachChecklist(title, this);
+        List<Pair<String, ArrayList<EachCourse>>> tmpChecklist = checklist.getCourse();
 
         /**
          * fake data
@@ -70,33 +75,33 @@ public class UserChecklistActivity extends AppCompatActivity {
         ArrayList<EachCourse> electiveunit = new ArrayList<>();
         ArrayList<EachCourse> nonmathunit = new ArrayList<>();
 
-        csunit.add(new EachCourse("17/18BCS", "CS 1[134]5", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 1[34]6", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 240", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 241", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 245", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 246", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 251", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 341", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 350", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 340-398; 440-489", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 340-398; 440-489", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 340-398; 440-489", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 440-489", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 440-489", false));
-        csunit.add(new EachCourse("17/18BCS", "CS 440-498 or CS 499T or CS 6xx or CS 7xx or CO 487 or STAT 440", false));
+        csunit.add(new EachCourse("17/18BCS", "CS 1[134]5", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 1[34]6", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 240", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 241", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 245", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 246", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 251", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 341", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 350", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 340-398; 440-489", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 340-398; 440-489", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 340-398; 440-489", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 440-489", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 440-489", false, true));
+        csunit.add(new EachCourse("17/18BCS", "CS 440-498 or CS 499T or CS 6xx or CS 7xx or CO 487 or STAT 440", false, true));
 
 
-        mathunit.add(new EachCourse("17/18BCS", "MATH 1[34]5", false));
-        mathunit.add(new EachCourse("17/18BCS", "MATH 1[34]6", false));
-        mathunit.add(new EachCourse("17/18BCS", "MATH 1[234]7", false));
-        mathunit.add(new EachCourse("17/18BCS", "MATH 1[234]8", false));
-        mathunit.add(new EachCourse("17/18BCS", "MATH 2[34]9", false));
-        mathunit.add(new EachCourse("17/18BCS", "STAT 2[34]0", false));
-        mathunit.add(new EachCourse("17/18BCS", "STAT 2[34]1", false));
+        mathunit.add(new EachCourse("17/18BCS", "MATH 1[34]5", false, true));
+        mathunit.add(new EachCourse("17/18BCS", "MATH 1[34]6", false, true));
+        mathunit.add(new EachCourse("17/18BCS", "MATH 1[234]7", false, true));
+        mathunit.add(new EachCourse("17/18BCS", "MATH 1[234]8", false, true));
+        mathunit.add(new EachCourse("17/18BCS", "MATH 2[34]9", false, true));
+        mathunit.add(new EachCourse("17/18BCS", "STAT 2[34]0", false, true));
+        mathunit.add(new EachCourse("17/18BCS", "STAT 2[34]1", false, true));
 
-        nonmathunit.add(new EachCourse("17/18BCS", "Communication list I", false));
-        nonmathunit.add(new EachCourse("17/18BCS", "Communication list II", false));
+        nonmathunit.add(new EachCourse("17/18BCS", "Communication list I", false, true));
+        nonmathunit.add(new EachCourse("17/18BCS", "Communication list II", false, true));
 
         requiredCourses.add(new Pair<>("CS Units", csunit));
         requiredCourses.add(new Pair<>("Math Units", mathunit));
@@ -214,7 +219,6 @@ public class UserChecklistActivity extends AppCompatActivity {
             ArrayList<EachCourse> courses = requiredCourses.get(i).second;
             int filled = courses.size();
             int totalCourses = (int) (total * 2);
-            System.out.println(totalCourses);
 
             for (int j = 0; j < totalCourses; ++j) {
 
