09fa813215d87b165a81b571596ea4a6031b9342
BenjaminLL <szyllll8@gmail.com>
2018-07-22 16:22:52 -0400

    add suggested courses for constraints

--- GoGrad/app/src/main/java/com/example/gograd/util/SuggestedCoursesModel.java
+++ GoGrad/app/src/main/java/com/example/gograd/util/SuggestedCoursesModel.java
@@ -6,10 +6,12 @@ import com.example.gograd.database.ChecklistOpenHelper;
 import com.example.gograd.database.DatabaseAccess;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SuggestedCoursesModel {
 
+    private DatabaseAccess databaseAccess;
     private ChecklistOpenHelper checklistOpenHelper;
     private String plan;
 
@@ -25,8 +27,10 @@ public class SuggestedCoursesModel {
 
         ArrayList<String> unCheckedCourses = checklistOpenHelper.getUncheck(plan);
 
-        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(context);
+        databaseAccess = DatabaseAccess.getInstance(context);
         databaseAccess.open();
+
+        boolean constraints = false;
 
         for (int i = 0; i < unCheckedCourses.size(); ++i) {
 
@@ -34,32 +38,126 @@ public class SuggestedCoursesModel {
 
             boolean isValid = databaseAccess.getIsValid(courseName);
 
-//            System.out.println(courseName + ": " + isValid);
             if (isValid) {
 
-                if (!databaseAccess.getNeedCheck(courseName)) {
+                boolean isSatisfies = testConditions(courseName);
+
+                if (isSatisfies) {
                     resultCourses.add(courseName);
-                    continue;
-                }
-
-                ArrayList<List<String>> orHave = databaseAccess.getOrHave(courseName);
-                List<String> mustHave = databaseAccess.getMustHave(courseName);
-
-                if (mustHave != null && !checkMustHave(mustHave) || !checkOrHave(orHave, courseName)) {
-                    continue;
                 } else {
-                    resultCourses.add(courseName);
+                    continue;
                 }
 
+            } else {
+
+                if (courseName.contains(":")) {
+                    constraints = true;
+                    continue;
+                }
+
+                if (constraints) {
+
+                    List<String> names = splitString(courseName);
+
+                    for (int j = 0; j < names.size(); ++j) {
+
+                        String name = names.get(j);
+                        isValid = databaseAccess.getIsValid(name);
+
+                        if (isValid) {
+
+                            boolean isSatisfies = testConditions(name);
+
+                            if (isSatisfies) {
+                                resultCourses.add(name);
+                            } else {
+                                continue;
+                            }
+                        }
+                    }
+                }
             }
         }
+
+        databaseAccess.close();
+    }
+
+    /**
+     * Test if the course satisfies all the prerequisite
+     */
+    private boolean testConditions(String courseName) {
+
+        if (!databaseAccess.getNeedCheck(courseName)) {
+            return true;
+        }
+
+        ArrayList<List<String>> orHave = databaseAccess.getOrHave(courseName);
+        List<String> mustHave = databaseAccess.getMustHave(courseName);
+
+        if (mustHave != null && !checkMustHave(mustHave) || !checkOrHave(orHave, courseName)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * split the string to a array of course names
+     */
+    private List<String> splitString(String courseName) {
+
+        List<String> courses = new ArrayList<>();
+
+        List<String> tmpStore = Arrays.asList(courseName.split(", "));
+
+        String courseTag = "";
+        String courseNum = "";
+
+        if (tmpStore.size() != 0) {
+
+            for (int i = 0; i < tmpStore.size(); ++i) {
+                String tmpName = tmpStore.get(i);
+                String[] splited = tmpName.split("\\s+");
+
+                if (splited.length > 1) {
+                    courseTag = splited[splited.length - 2];
+                    courseNum = splited[splited.length - 1];
+                } else if (splited.length == 1) {
+                    courseNum = splited[0];
+                }
+
+                if (!courseTag.equals("") && isNumeric(courseNum)) {
+                    courses.add(courseTag + " " + courseNum);
+                }
+
+                courseNum = "";
+            }
+        }
+
+        return courses;
+    }
+
+    /**
+     * Test if the string is a valid course num
+     */
+    private boolean isNumeric(String courseNum) {
+
+        try {
+            int num = Integer.parseInt(courseNum);
+        } catch (NumberFormatException ex) {
+
+            try {
+                int num = Integer.parseInt(courseNum.substring(0, courseNum.length()-1));
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private boolean checkOrHave(ArrayList<List<String>> orHave, String Name) {
 
-        if (Name.equals("MATH 1[34]6")) {
-            System.out.println(orHave.size());
-        }
         for (int i = 0; i < orHave.size(); ++i) {
             List<String> choices = orHave.get(i);
 
@@ -67,10 +165,6 @@ public class SuggestedCoursesModel {
 
                 String courseName = choices.get(j);
                 boolean checked = checklistOpenHelper.getIsCheck(plan, courseName);
-
-                if (Name.equals("MATH 1[34]6")) {
-                    System.out.println(courseName + ": " +checked);
-                }
 
                 if (checked) break;
                 if (!checked && j == choices.size() - 1) return false;
